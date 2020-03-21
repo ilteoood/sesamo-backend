83ad6dc627a77b2f54bd41b2e7843c61a5d7acae
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-03-21 21:15:20 +0100

    Action automatic deploy

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
-    process.env["GOOGLE_APPLICATION_CREDENTIALS"] = "./firebase_reader.json";
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 3000);
 }
