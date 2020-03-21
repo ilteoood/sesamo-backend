acfe465ba61b3a0ea2ea1ea75c4c9b5a8f5c8cc9
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-03-21 22:24:35 +0100

    Local development configurations

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS || "./firebase_reader.json";
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 3000);
 }
