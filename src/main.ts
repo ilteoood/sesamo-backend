3e3761d1e35da3c4992684c9fb5e625f2dd1350e
Matteo Pietro Dazzi <matteopietro.dazzi@gft.com>
2020-03-22 14:52:07 +0100

    Credentials for local development

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import * as fs from "fs";
+
+const FIREBASE_CREDENTIALS = "./firebase_reader.json";
 
 async function bootstrap() {
-    process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS || "./firebase_reader.json";
+    configureCredentials();
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT || 3000);
+}
+
+function configureCredentials() {
+    if (fs.existsSync(FIREBASE_CREDENTIALS)) {
+        process.env.GOOGLE_APPLICATION_CREDENTIALS = FIREBASE_CREDENTIALS;
+    }
 }
 
 bootstrap();
