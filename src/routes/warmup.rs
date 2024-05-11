4a6cce49cadfd385e8880caa2c29a8bd289205bf
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-05-11 18:13:03 +0200

    feat: test warmup handler

--- src/routes/warmup.rs
+++ src/routes/warmup.rs
@@ -4,3 +4,17 @@ use actix_web::{get, HttpResponse, Responder};
 async fn handler() -> impl Responder {
     HttpResponse::Ok()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[test]
+    async fn test_warmup_handler() {
+        let app = test::init_service(App::new().service(handler)).await;
+        let req = test::TestRequest::get().uri("/warmup").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), 200);
+    }
+}
