82d8a81a974a010f59b8b746dd66a0ba42ab1925
matteo.pietro.dazzi <matteo.pietro.dazzi@nearform.com>
2024-05-13 07:45:01 +0000

    fix: open handler

--- src/routes/open.rs
+++ src/routes/open.rs
@@ -3,7 +3,7 @@ use actix_web::{post, web, HttpResponse, Responder};
 use crate::models::{MessageResponse, OpenRequest};
 
 #[post("/{name}")]
-async fn handler(name: web::Path<String>, request_body: web::JsonBody<OpenRequest>) -> impl Responder {
+async fn handler(_name: web::Path<String>, _request_body: web::Json<OpenRequest>) -> impl Responder {
     let response = MessageResponse {
         message_id: "open_ok".to_string(),
     };
