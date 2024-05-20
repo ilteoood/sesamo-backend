2b950bdaa711c10202a97882c002bc2e9d4704a3
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-05-20 23:22:55 +0200

    fix: collect

--- src/firebase.rs
+++ src/firebase.rs
@@ -1,36 +1,139 @@
-use std::{env::set_var, path::Path};
-use firestore::{errors::FirestoreError, FirestoreDb};
-use futures::{stream::BoxStream, StreamExt};
+use firestore::FirestoreDb;
+use futures::{future, stream::BoxStream, StreamExt};
+use std::{collections::HashMap, env::set_var, error::Error, fs::File, io::BufReader, path::Path};
 
-use crate::models::ServerDocument;
+use crate::models::{
+    FirestoreServiceAccount, ObjectRequest, ServerDocument, ServerDocumentBase, ServerDocumentConfiguration
+};
 
-const FIREBASE_CREDENTIALS: &str = "./firebase_reader.json";
+const FIREBASE_CREDENTIALS: &str =
+    "/Users/ilteoood/Documents/git/personal/sesamo-backend/firebase_reader.json";
 
-fn configure_credentials() {
-    if Path::new(FIREBASE_CREDENTIALS).exists() {
-        set_var("GOOGLE_APPLICATION_CREDENTIALS", FIREBASE_CREDENTIALS)
+const SERVERS_COLLECTION: &str = "servers";
+
+struct Firestore {
+    firestore_db: FirestoreDb,
+    server_map: HashMap<String, ServerDocument>,
+}
+
+impl Firestore {
+    async fn new() -> Result<Firestore, Box<dyn Error>> {
+        Self::configure_credentials();
+
+        let firestore_service_account = Self::read_service_account()?;
+
+        let firestore_db = FirestoreDb::new(firestore_service_account.project_id.as_str()).await?;
+
+        let server_map = Self::build_server_map(&firestore_db).await?;
+
+        Ok(Self {
+            firestore_db,
+            server_map,
+        })
+    }
+
+    fn configure_credentials() {
+        if Path::new(FIREBASE_CREDENTIALS).exists() {
+            set_var("GOOGLE_APPLICATION_CREDENTIALS", FIREBASE_CREDENTIALS)
+        }
+    }
+
+    fn read_service_account() -> Result<FirestoreServiceAccount, Box<dyn Error>> {
+        let file = File::open(FIREBASE_CREDENTIALS)?;
+        let reader = BufReader::new(file);
+
+        Ok(serde_json::from_reader(reader).unwrap())
+    }
+
+    async fn build_server_map(
+        firestore_db: &FirestoreDb,
+    ) -> Result<HashMap<String, ServerDocument>, Box<dyn Error>> {
+        let server_documents: Vec<ServerDocumentBase> = firestore_db
+            .fluent()
+            .list()
+            .from(SERVERS_COLLECTION)
+            .obj()
+            .stream_all()
+            .await?
+            .collect()
+            .await;
+
+        let servers: Vec<ServerDocument> = future::join_all(
+            server_documents
+                .into_iter()
+                .map(|doc| Self::enrich_document(firestore_db, doc)),
+        )
+        .await;
+
+        let server_map = servers
+            .into_iter()
+            .map(|doc| (doc.id.clone(), doc))
+            .collect();
+
+        Ok(server_map)
+    }
+
+    async fn enrich_document(firestore_db: &FirestoreDb, doc: ServerDocumentBase) -> ServerDocument {
+        let parent_path = firestore_db
+            .parent_path(SERVERS_COLLECTION, doc.id.clone()).unwrap();
+
+        let allowed_devices: Vec<String> = firestore_db
+            .fluent()
+            .select()
+            .by_id_in("configurations")
+            .parent(&parent_path)
+            .obj()
+            .one("allowedDevices")
+            .await
+            .unwrap()
+            .unwrap();
+
+        let gate: ObjectRequest = firestore_db
+            .fluent()
+            .select()
+            .by_id_in("configurations")
+            .parent(&parent_path)
+            .obj()
+            .one("gate")
+            .await
+            .unwrap()
+            .unwrap();
+
+        let wicket: ObjectRequest = firestore_db
+            .fluent()
+            .select()
+            .by_id_in("configurations")
+            .parent(&parent_path)
+            .obj()
+            .one("wicket")
+            .await
+            .unwrap()
+            .unwrap();
+
+        let configurations = ServerDocumentConfiguration {
+            allowed_devices,
+            gate,
+            wicket,
+        };
+
+        ServerDocument {
+            configurations,
+            id: doc.id,
+            name: doc.name,
+            r#type: doc.r#type,
+        }
     }
 }
 
-pub async fn create() -> Result<FirestoreDb, FirestoreError> {
-    configure_credentials();
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::test;
 
-    let firestore_db = FirestoreDb::new("sesamo-iot").await?;
+    #[test]
+    async fn test_configure_credentials() {
+        let result = Firestore::new().await;
 
-    let objs_stream: BoxStream<ServerDocument> = firestore_db
-        .fluent()
-        .list()
-        .from("servers")
-        .obj()
-        .stream_all()
-        .await?;
-
-    let as_vec: Vec<ServerDocument> = objs_stream.collect().await;
-    println!("{:?}", as_vec);
-
-
-
-    let db = Firestore::new();
-
-    Ok(firestore_db)
+        assert!(result.is_ok());
+    }
 }
--- src/models.rs
+++ src/models.rs
@@ -14,9 +14,59 @@ pub struct OpenRequest {
     pub server_id: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct ObjectRequest {
+    pub body: String,
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ServerDocumentConfiguration {
+    #[serde(rename = "allowedDevices")]
+    pub allowed_devices: Vec<String>,
+    pub gate: ObjectRequest,
+    pub wicket: ObjectRequest
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ServerDocumentBase {
+    #[serde(alias = "_firestore_id")]
+    pub id: String,
+    pub name: String,
+    pub r#type: String,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct ServerDocument {
+    #[serde(alias = "_firestore_id")]
+    pub id: String,
     pub name: String,
     pub r#type: String,
+    pub configurations: ServerDocumentConfiguration
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct FirestoreServiceAccount {
+  #[serde(rename = "type")]
+  pub service_account_type: String,
+  #[serde(rename = "project_id")]
+  pub project_id: String,
+  #[serde(rename = "private_key_id")]
+  pub private_key_id: String,
+  #[serde(rename = "private_key")]
+  pub private_key: String,
+  #[serde(rename = "client_email")]
+  pub client_email: String,
+  #[serde(rename = "client_id")]
+  pub client_id: String,
+  #[serde(rename = "auth_uri")]
+  pub auth_uri: String,
+  #[serde(rename = "token_uri")]
+  pub token_uri: String,
+  #[serde(rename = "auth_provider_x509_cert_url")]
+  pub auth_provider_x509_cert_url: String,
+  #[serde(rename = "client_x509_cert_url")]
+  pub client_x509_cert_url: String,
+  #[serde(rename = "universe_domain")]
+  pub universe_domain: String,
 }
